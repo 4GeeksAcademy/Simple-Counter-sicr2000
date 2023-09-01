c237e1acf551a2a859d9837390b3315c52d5fb4a
bynx <sebastiancastrorajbe@gmail.com>
2023-08-31 22:34:27 -0400

    First commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,37 @@
 import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faClock } from "@fortawesome/free-solid-svg-icons";
 
 //create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+const Home = (props) => {
+  let digitsView = [];
+  const secondsDigits = String(props.loadSeconds).padStart(6, "0");
+  for (const digit of secondsDigits) {
+    digitsView = digitsView.concat(
+      <div
+        className="border border-white rounded-2 px-3 mx-1"
+        style={{ fontFamily: "monospace", fontSize: "70px" }}
+      >
+        {digit}
+      </div>
+    );
+  }
+
+  return (
+    <div
+      className="d-flex justify-content-center text-center text-white col-6 m-auto"
+      style={{ backgroundColor: "black", padding: "40px" }}
+    >
+      <div
+        className="border border-white rounded-2 px-3 mx-1"
+        style={{ fontSize: "70px" }}
+      >
+        <FontAwesomeIcon icon={faClock} />
+      </div>
+      {digitsView}
+    </div>
+  );
 };
 
 export default Home;
--- src/js/index.js
+++ src/js/index.js
@@ -8,5 +8,16 @@ import "../styles/index.css";
 //import your own components
 import Home from "./component/home.jsx";
 
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+let seconds = 0;
+function nextSecond() {
+  seconds++;
+  ReactDOM.render(
+    <Home loadSeconds={seconds} />,
+    document.querySelector("#app")
+  );
+}
+
+window.onload = () => {
+  ReactDOM.render(<Home loadSeconds={0} />, document.querySelector("#app"));
+  setInterval(nextSecond, 1000);
+};
